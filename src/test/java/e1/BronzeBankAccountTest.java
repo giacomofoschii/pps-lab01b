e1f74acbdea91be773f32ed0727acb1b781e7b29
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-03-03 14:01:09 +0100

    Added BronzeBankAccount tests, tdd red

--- src/test/java/e1/BronzeBankAccountTest.java
+++ src/test/java/e1/BronzeBankAccountTest.java
@@ -7,25 +7,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SilverBankAccountTest extends AbstractBankAccountTest{
+public class BronzeBankAccountTest extends AbstractBankAccountTest {
     public static final int FIRST_WITHDRAW = 200;
-    public static final int SECOND_WITHDRAW = 1200;
+    public static final int SECOND_WITHDRAW = 50;
+    public static final int THIRD_WITHDRAW = 1200;
     private static final int EXPECTED_BALANCE = INITIAL_DEPOSIT - FIRST_WITHDRAW - 1;
 
     @BeforeEach
     void init() {
-        this.account = new BankAccountFactoryImpl().createSilverBankAccount();
+        this.account = new BankAccountFactoryImpl().createBronzeBankAccount();
     }
 
     @Test
-    void testCanWithdraw() {
+    void testConditionalFeeWithdraw() {
         depositAndWithdraw(INITIAL_DEPOSIT, FIRST_WITHDRAW);
         assertEquals(EXPECTED_BALANCE, this.account.getBalance());
+        this.account.withdraw(SECOND_WITHDRAW);
+        assertEquals(EXPECTED_BALANCE - SECOND_WITHDRAW, this.account.getBalance());
     }
 
     @Test
     void testCannotWithdrawMoreThanAvailable(){
         this.account.deposit(INITIAL_DEPOSIT);
-        assertThrows(IllegalStateException.class, () -> this.account.withdraw(SECOND_WITHDRAW));
+        assertThrows(IllegalStateException.class, () -> this.account.withdraw(THIRD_WITHDRAW));
     }
 }
